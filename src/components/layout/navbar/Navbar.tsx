2e172681307cffb74fa57768b80664869bcadba9
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-03 20:29:30 -0300

    platform navbar and it's functionalities

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,8 +1,28 @@
-function Navbar() {
+import React from 'react';
+import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
 
-    return(
-        <h1>This is Navbar component</h1>
-    );
+class NavbarComponent extends React.Component {
+
+    public handleLogout(): void {
+        window.sessionStorage.clear();
+    }
+
+    render() {
+        return(
+            <nav className="navbar">
+                <div className="logo-block">
+                    <span className="logo">SPS Consultoria</span>
+                </div>
+                <div className="logout-block">
+                    <NavLink onClick={this.handleLogout} className="logout" to='/login' style={{ textDecoration: 'none' }}>
+                        Sair
+                    </NavLink>
+                </div>
+            </nav>
+        );
+    }
 }
 
-export default Navbar;
+export default NavbarComponent;
