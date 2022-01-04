a21b4fcb2715fb6eb4ee71c976b25b0fa0652d1d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-03 21:12:54 -0300

    repos search component layout

--- src/components/entity/repo/repo-search/RepoSearchComponent.tsx
+++ src/components/entity/repo/repo-search/RepoSearchComponent.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import './reposearch.css';
 
 class RepoSearchComponent extends React.Component {
     render() {
         return(
+            <form className="d-flex justify-content-center">
+                <span className="text">Pesquisar</span>
+                <input id="search" type="text" className="new-repo-input" placeholder="Encontre um repositório..." name="repoUrl"></input>
+                <button className="new-repo-button">Pesquisar</button>
+            </form>
         );
     }
 
