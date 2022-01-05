6d8cd1bbb13b7037cc2e460a91ba6c86c7abd885
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-05 02:53:30 -0300

    adding new repo to json-server using context

--- src/components/base/repo-add/RepoAdd.jsx
+++ src/components/base/repo-add/RepoAdd.jsx
@@ -1,10 +1,35 @@
+import { useContext, useState } from 'react';
+import RepoContext from '../../context/RepoContext';
 import '../repo/Repo.css';
 
 function RepoAdd() {
+    const [text, setText] = useState('');
+    const {addRepo} = useContext(RepoContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addRepo(text);
+        
+        setText('');
+    }
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
     return (
-        <form className="d-flex justify-content-center">
+        <form 
+            className="d-flex justify-content-center"
+            onSubmit={handleSubmit}
+            >
             <span className="text">Pesquisar</span>
-            <input id="search" type="text" className="new-repo-input" placeholder="Encontre um repositório..." name="repoUrl" />
+            <input
+                onChange={handleChange}
+                id="search"
+                className="new-repo-input" 
+                placeholder="Encontre um repositório..."
+                value={text} 
+             />
             <button className="new-repo-button">Pesquisar</button>
         </form>
     )
--- src/components/context/RepoContext.jsx
+++ src/components/context/RepoContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { extractNameAndOwner } from '../shared/functions/ExtractNameAndOwner';
 
 const RepoContext = createContext();
 
@@ -16,9 +17,25 @@ export const RepoProvider = ( {children} ) => {
         setRepos(data);
     }
 
+    const addRepo = async (repoUrl) => {
+        const newRepo = extractNameAndOwner(repoUrl)
+        const response = await fetch('/repos', {
+            method: 'POST',
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newRepo)
+        });
+        const data = await response.json();
+        console.log(data);
+        setRepos([data, ...repos]);
+    }
+
     return(
         <RepoContext.Provider value={{
-            repos: repos
+            repos: repos,
+            addRepo: addRepo
         }}>
             {children}
         </RepoContext.Provider>
