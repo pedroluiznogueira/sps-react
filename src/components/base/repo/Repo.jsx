189da5d12af497f499270fd9df9180ddea0c8e98
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-05 01:39:41 -0300

    refactoring repos components structure

--- src/components/base/repo/Repo.jsx
+++ src/components/base/repo/Repo.jsx
@@ -1,39 +1,20 @@
 import React from "react";
 import './Repo.css';
+import RepoList from './../repo-list/RepoList';
+import RepoAdd from "../repo-add/RepoAdd";
+import RepoSearch from "../repo-search/RepoSearch";
 
 function RepoComponent() {
 
     return(
         <div className="container">
             <section id="sec" className="main">
-                <form className="d-flex justify-content-center">
-                    <span className="text">Pesquisar</span>
-                    <input id="search" type="text" className="new-repo-input" placeholder="Encontre um repositório..." name="repoUrl" />
-                    <button className="new-repo-button">Pesquisar</button>
-                </form>
-
-                <div className="repos-list">
-                    <h3>Repositórios</h3>
-                        <ul>
-                            <li>
-                                <div className="left-block">
-                                    <span className="repo-owner"></span>
-                                    <span className="repo-name"><b></b></span>
-                                </div>
-                                <div className="right-block">
-                                <i className="material-icons">delete</i>
-                                </div>
-                            </li>
-                        </ul>
-                </div> 
+                <RepoSearch />
+                <RepoList />
             </section>
             
             <section className="bottom">
-                <form className="d-flex justify-content-center">
-                    <span className="text">Pesquisar</span>
-                    <input id="search" type="text" className="new-repo-input" placeholder="Encontre um repositório..." name="repoUrl" />
-                    <button className="new-repo-button">Pesquisar</button>
-                </form>
+                <RepoAdd />
             </section>
         </div>
     );
